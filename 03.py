3f61fdbb236e88d06af650f4201d469807d146ae
Daniel Schulz <daniel.schulz@a3rtgm.de>
2023-11-27 13:25:55 +0100

    Add explanatory comments

--- 03.py
+++ 03.py
@@ -1,13 +1,22 @@
 import turtle
 
+# set the color of the line to "red" (1st parameter) and the fill to "yellow" (2nd parameter)
 turtle.color('red', 'yellow')
+
+# tell the turtle to fill the shape that it draws
 turtle.begin_fill()
 
+# create a loop that just keeps running ("True") no matter what (unless actively ended with "break")
 while True:
+    # go 200px forward and then turn left 110°
     turtle.forward(200)
     turtle.left(110)
+
+    # when the turtle's position is smaller than 1px in any direction (meaning when it is back where it started)
+    # break the loop
     if abs(turtle.pos()) < 1:
         break
 
+# end the fill and apply the color
 turtle.end_fill()
 turtle.done()

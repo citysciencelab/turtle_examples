23a2ece0b12f4f7b79509e88f3a84ce0bb81900f
Daniel Schulz <daniel.schulz@a3rtgm.de>
2023-11-27 12:25:28 +0100

    Add explanatory comments

--- 02.py
+++ 02.py
@@ -4,6 +4,7 @@ import random
 hans = turtle.Turtle()
 hans.shape("turtle") 
 
+# Define functions for facing any direction and walking 100px that way
 def up():
     hans.setheading(90)
     hans.forward(100)
@@ -20,16 +21,20 @@ def right():
     hans.setheading(0)
     hans.forward(100)
 
+# Define a list of colors
 colors = ["red", "blue", "green", "yellow", "black", "pink"]
 
+# Define function for changing color at random
 def clickLeft(x, y):  # Make sure to have parameters x, y
     hans.color(random.choice(colors))
 
+# Define function for leaving a stamp at the current location
 def clickRight(x, y):
     hans.stamp()
 
 turtle.listen()
 
+# Bind the above functions to the arrow keys and the mouse buttons
 turtle.onscreenclick(clickLeft, 1)  # 1:Left Mouse Button, 2: Middle Mouse Button
 turtle.onscreenclick(clickRight, 3) # 3: Right Mouse Button
 

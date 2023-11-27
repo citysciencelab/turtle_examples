2cdd892c759d0f9a7026f9cb38149425fa272bf1
Daniel Schulz <daniel.schulz@a3rtgm.de>
2023-11-27 12:23:13 +0100

    add explanatory comments

--- 01.py
+++ 01.py
@@ -1,20 +1,30 @@
 import turtle
 import random
 
+# Create a new turtle and store it to a variable, i.e. give it the name "hans" by which it can be called later
 hans = turtle.Turtle()
+
+# Set the shape of our turtle to be a "turtle" (This is a function built into the turtle library)
 hans.shape("turtle")
 
-# hans.penup()
-# hans.goto(-120, -150)
-# hans.pendown()
+# Create a variable that stores whether Hans has returned to his origin and set it to initially be false
+hans_returned_home = False
 
-while True:
+# Create a loop that keeps running as long Hans is not back home
+while not hans_returned_home:
+    # Let Hans walk forward for 100px
     hans.forward(100)
+    
+    # if Hans is further away that 200px from home (the origin of his journey at the coordinates (0,0))
+    # let him turn left or right randomly with a 50% chance
+    # choosing the angle of the turn to be random between 0 and 180 degrees
     if turtle.distance((hans.pos()[0], hans.pos()[1]), (0,0)) > 200:
-        # turtle.circle(50, 180)
         if random.random() >= 0.5:
-            hans.left(random.random() * 120)
+            hans.left(random.random() * 180)
         else:
-            hans.right(random.random() * 120)
+            hans.right(random.random() * 180)
+    # if Hans happens to get as close to home as 10px distance we end the loop by setting hans_returned_home to be true
+    if turtle.distance((hans.pos()[0], hans.pos()[1]), (0,0)) < 10:
+        hans_returned_home = True
 
 turtle.done()
